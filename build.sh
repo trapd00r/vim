4e060f81d154068948769975bbc277178f412cc6
Magnus Woldrich <m@japh.se>
2012-06-25 18:46:26 +0200

    build.sh: rewritten and retested, this seems to work consistently

--- build.sh
+++ build.sh
@@ -1,31 +1,31 @@
-make distclean
-
-export CPP_FLAGS="-DUSR_VIMRC_FILE=$XDG_CONFIG_HOME/vimrc -DUSR_EXRC_FILE=$XDG_CONFIG_HOME/exrc"
+#!/bin/sh
+[[ $1 ]] && make distclean
 
 sh configure \
-  --disable-acl                             \
-  --disable-darwin                          \
-  --disable-gpm                             \
-  --disable-gtk2-check                      \
-  --disable-gtktest                         \
-  --disable-gui                             \
-  --disable-largefile                       \
-  --disable-netbeans                        \
-  --disable-option-checking                 \
-  --disable-selinux                         \
-  --disable-sniff                           \
-  --disable-sysmouse                        \
-  --disable-workshop                        \
-  --disable-xim                             \
-  --disable-xsmp                            \
-  --enable-fontset                          \
-  --enable-multibyte                        \
-  --enable-perlinterp                       \
-  --enable-python3interp=yes                \
-  --enable-pythoninterp=yes                 \
-  --enable-rubyinterp=yes                   \
-  --with-compiledby='Magnus Woldrich'       \
-  --with-features=huge                      \
-  --with-global-runtime=/home/scp1/etc/vim  \
-  --with-vim-name=vi                        \
-  --without-x && make && ./src/vi #&& su -c 'make install; unlink /usr/bin/vim; ln -s /usr/local/bin/vim /usr/bin/'
+  --disable-acl                                    \
+  --disable-darwin                                 \
+  --disable-gpm                                    \
+  --disable-gtk2-check                             \
+  --disable-gtktest                                \
+  --disable-gui                                    \
+  --disable-largefile                              \
+  --disable-netbeans                               \
+  --disable-option-checking                        \
+  --disable-selinux                                \
+  --disable-sniff                                  \
+  --disable-sysmouse                               \
+  --disable-workshop                               \
+  --disable-xim                                    \
+  --disable-xsmp                                   \
+  --enable-fontset                                 \
+  --enable-multibyte                               \
+  --disable-perlinterp                             \
+  --disable-python3interp                          \
+  --disable-pythoninterp                           \
+  --disable-rubyinterp                             \
+  --disable-option-checking                        \
+  --with-compiledby="wolly - $(date)"              \
+  --with-features=huge                             \
+  --with-vim-name="wim-$(date +%d%H%M)"            \
+  --with-global-runtime="${HOME}/dev/vim/runtime/" \
+  --without-x && /usr/bin/make
